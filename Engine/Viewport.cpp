1c65094150c8587158e103637fada5b0b2601ba6
NorbertWychowski <norbertwychowski@gmail.com>
2017-10-15 14:39:05 +0200

    removed goto

--- Engine/Viewport.cpp
+++ Engine/Viewport.cpp
@@ -13,17 +13,17 @@ Viewport::~Viewport() {
 void Viewport::drawViewport(BITMAP * bitmap, int color, LineStyle lineStyle) {
 	LineSegment::drawLineS(bitmap, Point2D(firstCorner.getX(), firstCorner.getY()),
 		Point2D(firstCorner.getX(), oppositeCorner.getY()), color, lineStyle);
-	LineSegment::drawLineS(bitmap, Point2D(firstCorner.getX(), firstCorner.getY()), 
+	LineSegment::drawLineS(bitmap, Point2D(firstCorner.getX(), firstCorner.getY()),
 		Point2D(oppositeCorner.getX(), firstCorner.getY()), color, lineStyle);
-	LineSegment::drawLineS(bitmap, Point2D(oppositeCorner.getX(), oppositeCorner.getY()), 
+	LineSegment::drawLineS(bitmap, Point2D(oppositeCorner.getX(), oppositeCorner.getY()),
 		Point2D(firstCorner.getX(), oppositeCorner.getY()), color, lineStyle);
-	LineSegment::drawLineS(bitmap, Point2D(oppositeCorner.getX(), oppositeCorner.getY()), 
+	LineSegment::drawLineS(bitmap, Point2D(oppositeCorner.getX(), oppositeCorner.getY()),
 		Point2D(oppositeCorner.getX(), firstCorner.getY()), color, lineStyle);
 }
 
 void Viewport::drawViewport(BITMAP * bitmap, float r, float g, float b, LineStyle lineStyle) {
 	LineSegment::drawLineS(bitmap, Point2D(firstCorner.getX(), firstCorner.getY()),
-		Point2D(firstCorner.getX(), oppositeCorner.getY()), makecol(r*255, g*255, b*255), lineStyle);
+		Point2D(firstCorner.getX(), oppositeCorner.getY()), makecol(r * 255, g * 255, b * 255), lineStyle);
 	LineSegment::drawLineS(bitmap, Point2D(firstCorner.getX(), firstCorner.getY()),
 		Point2D(oppositeCorner.getX(), firstCorner.getY()), makecol(r * 255, g * 255, b * 255), lineStyle);
 	LineSegment::drawLineS(bitmap, Point2D(oppositeCorner.getX(), oppositeCorner.getY()),
@@ -65,79 +65,75 @@ std::vector<LineSegment> Viewport::cutLine(LineSegment line) {
 	char codeP1;
 	char codeP2;
 
-	//hehe
-step1:
-	codeP1 = 0;
-	codeP2 = 0;
-	if (x1 < firstCorner.getX()) {
-		codeP1 += (1 << 0);
-	}
-	else if (x1 > oppositeCorner.getX()) {
-		codeP1 += (1 << 1);
-	}
-	if (y1 > oppositeCorner.getY()) {
-		codeP1 += (1 << 2);
-	}
-	else if (y1 < firstCorner.getY()) {
-		codeP1 += (1 << 3);
-	}
-
-	if (x2 < firstCorner.getX()) {
-		codeP2 += (1 << 0);
-	}
-	else if (x2 > oppositeCorner.getX()) {
-		codeP2 += (1 << 1);
-	}
-	if (y2 > oppositeCorner.getY()) {
-		codeP2 += (1 << 2);
-	}
-	else if (y2 < firstCorner.getY()) {
-		codeP2 += (1 << 3);
-	}
-
-	if ((codeP1 == 0) && (codeP2 == 0)) {
-		LineSegment l = line;
-		l.setP1(Point2D(x1, y1));
-		l.setP2(Point2D(x2, y2));
-		tmp.push_back(l);
-		tmp.push_back(line);
-
-		return tmp;
-	}
-	else if ((codeP1 & codeP2) != 0) {
-		LineSegment l = line;
-		l.setP1(Point2D(0, 0));
-		l.setP2(Point2D(0, 0));
-		tmp.push_back(l);
-		tmp.push_back(line);
-		return tmp;
-	}
-	else {
-		if (codeP1 == 0) {
-			swap(codeP1, codeP2);
-			swap(x1, x2);
-			swap(y1, y2);
+	while (true) {
+		codeP1 = 0;
+		codeP2 = 0;
+		if (x1 < firstCorner.getX()) {
+			codeP1 += (1 << 0);
+		}
+		else if (x1 > oppositeCorner.getX()) {
+			codeP1 += (1 << 1);
+		}
+		if (y1 > oppositeCorner.getY()) {
+			codeP1 += (1 << 2);
+		}
+		else if (y1 < firstCorner.getY()) {
+			codeP1 += (1 << 3);
 		}
 
-		if ((codeP1 & (1 << 0)) == (1 << 0)) {
-			y1 = y1 + (firstCorner.getX() - x1)*(y2 - y1) / (x2 - x1);
-			x1 = firstCorner.getX();
-			goto step1;
+		if (x2 < firstCorner.getX()) {
+			codeP2 += (1 << 0);
 		}
-		if ((codeP1 & (1 << 1)) == (1 << 1)) {
-			y1 = y1 + (oppositeCorner.getX() - x1)*(y2 - y1) / (x2 - x1);
-			x1 = oppositeCorner.getX();
-			goto step1;
+		else if (x2 > oppositeCorner.getX()) {
+			codeP2 += (1 << 1);
 		}
-		if ((codeP1 & (1 << 2)) == (1 << 2)) {
-			x1 = x1 + (oppositeCorner.getY() - y1)*(x2 - x1) / (y2 - y1);
-			y1 = oppositeCorner.getY();
-			goto step1;
+		if (y2 > oppositeCorner.getY()) {
+			codeP2 += (1 << 2);
 		}
-		if ((codeP1 & (1 << 3)) == (1 << 3)) {
-			x1 = x1 + (firstCorner.getY() - y1)*(x2 - x1) / (y2 - y1);
-			y1 = firstCorner.getY();
-			goto step1;
+		else if (y2 < firstCorner.getY()) {
+			codeP2 += (1 << 3);
+		}
+
+		if ((codeP1 == 0) && (codeP2 == 0)) {
+			LineSegment l = line;
+			l.setP1(Point2D(x1, y1));
+			l.setP2(Point2D(x2, y2));
+			tmp.push_back(l);
+			tmp.push_back(line);
+
+			return tmp;
+		}
+		else if ((codeP1 & codeP2) != 0) {
+			LineSegment l = line;
+			l.setP1(Point2D(0, 0));
+			l.setP2(Point2D(0, 0));
+			tmp.push_back(l);
+			tmp.push_back(line);
+			return tmp;
+		}
+		else {
+			if (codeP1 == 0) {
+				swap(codeP1, codeP2);
+				swap(x1, x2);
+				swap(y1, y2);
+			}
+
+			if ((codeP1 & (1 << 0)) == (1 << 0)) {
+				y1 = y1 + (firstCorner.getX() - x1)*(y2 - y1) / (x2 - x1);
+				x1 = firstCorner.getX();
+			}
+			if ((codeP1 & (1 << 1)) == (1 << 1)) {
+				y1 = y1 + (oppositeCorner.getX() - x1)*(y2 - y1) / (x2 - x1);
+				x1 = oppositeCorner.getX();
+			}
+			if ((codeP1 & (1 << 2)) == (1 << 2)) {
+				x1 = x1 + (oppositeCorner.getY() - y1)*(x2 - x1) / (y2 - y1);
+				y1 = oppositeCorner.getY();
+			}
+			if ((codeP1 & (1 << 3)) == (1 << 3)) {
+				x1 = x1 + (firstCorner.getY() - y1)*(x2 - x1) / (y2 - y1);
+				y1 = firstCorner.getY();
+			}
 		}
 	}
 	return tmp;
